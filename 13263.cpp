e73696ac0569b0828f2dfbb8eb36428f174a8636
Jeounghui Nah <jhnah917@naver.com>
2019-01-27 05:14:08 +0900

    Create 13263.cpp

---
+++ 13263.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class LinearCHT{
+	public:
+		vector< pair<T, T> > stk;
+		long long idx;
+
+		double cross(long long a, long long b){
+			double t1 = (double)(stk[b].second - stk[a].second);
+			double t2 = (double)(stk[a].first - stk[b].first);
+			return t1/t2;
+		}
+
+		void insert(T a, T b){
+			stk.emplace_back(a, b);
+			while(stk.size() > 2 &&
+			cross(stk.size()-3, stk.size()-2) > cross(stk.size()-2, stk.size()-1)){
+				stk[stk.size()-2] = stk.back();
+				stk.pop_back();
+			}
+		}
+
+		T query(T x){
+			while(idx+1 < stk.size() && cross(idx, idx+1) <= x){
+				idx++;
+			}
+			return x*stk[idx].first + stk[idx].second;
+		}
+
+		LinearCHT(){
+			idx = 0; stk.clear();
+		}
+};
+
+typedef long long ll;
+typedef pair<ll, ll> p;
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	LinearCHT<ll> cht;
+
+	int n; cin >> n;
+	vector<p> v(n);
+	for(int i=0; i<n; i++) cin >> v[i].first;
+	for(int i=0; i<n; i++) cin >> v[i].second;
+
+	vector<ll> dp(n);
+	cht.insert(v[0].second, 0);
+	for(int i=1; i<n; i++){
+		dp[i] = cht.query(v[i].first);
+		cht.insert(v[i].second, dp[i]);
+	}
+	cout << dp[n-1];
+}
